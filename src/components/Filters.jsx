4e9d2433280773525635398645c8d3e061b893a8
Aman Raj <amanraj5687@gmail.com>
2025-10-19 17:04:48 +0530

    Updated Filters component with new location and industry options

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -3,7 +3,8 @@ import { TextField, MenuItem, Box } from "@mui/material";
 
 const Filters = ({ filters, onFilterChange }) => {
   return (
-    <Box display="flex" gap={2} my={2}>
+    <Box display="flex" flexWrap="wrap" gap={2} my={2}>
+      {/* Search by Name */}
       <TextField
         label="Search by Name"
         variant="outlined"
@@ -12,6 +13,8 @@ const Filters = ({ filters, onFilterChange }) => {
         value={filters.name}
         onChange={onFilterChange}
       />
+
+      {/* Filter by Location */}
       <TextField
         select
         label="Filter by Location"
@@ -21,11 +24,13 @@ const Filters = ({ filters, onFilterChange }) => {
         onChange={onFilterChange}
       >
         <MenuItem value="">All</MenuItem>
-        <MenuItem value="Bangalore">Bangalore</MenuItem>
-        <MenuItem value="Hyderabad">Hyderabad</MenuItem>
-        <MenuItem value="Pune">Pune</MenuItem>
-        <MenuItem value="Mumbai">Mumbai</MenuItem>
+        <MenuItem value="California">California</MenuItem>
+        <MenuItem value="Seattle">Seattle</MenuItem>
+        <MenuItem value="Washington">Washington</MenuItem>
+        <MenuItem value="Texas">Texas</MenuItem>
       </TextField>
+
+      {/* Filter by Industry */}
       <TextField
         select
         label="Filter by Industry"
@@ -35,10 +40,9 @@ const Filters = ({ filters, onFilterChange }) => {
         onChange={onFilterChange}
       >
         <MenuItem value="">All</MenuItem>
-        <MenuItem value="Software">Software</MenuItem>
-        <MenuItem value="Healthcare">Healthcare</MenuItem>
-        <MenuItem value="Education">Education</MenuItem>
-        <MenuItem value="Finance">Finance</MenuItem>
+        <MenuItem value="Technology">Technology</MenuItem>
+        <MenuItem value="E-commerce">E-commerce</MenuItem>
+        <MenuItem value="Automotive">Automotive</MenuItem>
       </TextField>
     </Box>
   );
